fe6e52b0390ad2c3bfeed204819e9458bc00a1b3
Joao-Machado-UAb <1300213@estudante.uab.pt>
2024-11-20 22:13:26 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-app.py - O ficheiro Python principal para o Web Service:
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +7,5 @@ def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
+  
